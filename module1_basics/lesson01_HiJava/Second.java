465dec4ca75ca0f84467101297b0c4982e875b73
katerina-creator <53622169+katerina-creator@users.noreply.github.com>
2025-10-15 13:30:47 -0700

    Create Second.java
    class Second

---
+++ module1_basics/lesson01_HiJava/Second.java
@@ -0,0 +1,24 @@
+import java.time.LocalDateTime;
+
+public class Second {
+    public static void main(String[] args) {
+        // It's comment. It doesn't compile.
+        // Date and Time
+        System.out.println(LocalDateTime.now());
+
+                // println prints text and MOVES the cursor to a new line
+        System.out.println("Hello");
+        System.out.println("World");
+
+        // print prints text but DOES NOT move to a new line
+        System.out.print("Hello ");
+        System.out.print("World");
+
+        // Print an empty line to separate the output
+        System.out.println();
+
+        // The \n symbol means "new line" (Line Feed)
+        // Here we use print, but it still moves to the next line because of \n
+        System.out.print("Java\nProgramming");
+    }
+}
